c8d8a65bbceadbe4b9ebf5eb55c55ff91bf053fa
H-Hirata9 <nov26hihi@gmail.com>
2022-07-12 19:04:40 +0900

    Add ALLOWED_HOST env

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
